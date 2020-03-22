3e266d0a7e34861fcc85d2df1a73ae7f2eb46fb8
dzucconi <mail@damonzucconi.com>
2020-03-22 16:58:53 -0400

    feat: implements aspect ratio box

---
+++ src/AspectRatioBox/AspectRatioBox.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { States } from "storybook-states";
+import { AspectRatioBox, AspectRatioBoxProps } from "./AspectRatioBox";
+
+export default { title: "AspectRatioBox", component: AspectRatioBox };
+
+export const Default = () => (
+  <States<Partial<AspectRatioBoxProps>>
+    states={[
+      { maxHeight: 200, maxWidth: 200 },
+      { maxHeight: 400, maxWidth: 400 },
+      { maxHeight: 600, maxWidth: 600 },
+      { maxHeight: 100, maxWidth: 600 }
+    ]}
+  >
+    <AspectRatioBox
+      aspectWidth={300}
+      aspectHeight={400}
+      maxHeight={200}
+      maxWidth={200}
+      bg="lightgray"
+    />
+  </States>
+);
