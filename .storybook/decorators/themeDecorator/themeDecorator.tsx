3849814eeabd88a5db8aa91784c25c746d295e67
dzucconi <mail@damonzucconi.com>
2020-06-22 09:50:48 -0400

    chore(storybook): handles theme for storybook-states

--- .storybook/decorators/themeDecorator/themeDecorator.tsx
+++ .storybook/decorators/themeDecorator/themeDecorator.tsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { DecoratorFn } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
+import { StylesProvider } from "storybook-states";
 import { GlobalStyles } from "../../../src/GlobalStyles";
 import { useThemer, ThemerProvider } from "../../../src/Themer";
 
 const Themed: React.FC = ({ children }) => {
   const { theme } = useThemer();
+
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
+      <StylesProvider
+        styles={{
+          state: {
+            border: `1px dotted ${theme.colors.hint}`,
+            padding: theme.space[3],
+            marginBottom: "1rem",
+          },
+          stateProps: {
+            display: "block",
+            marginTop: theme.space[3],
+            paddingTop: theme.space[3],
+            fontFamily: theme.fonts.mono,
+            fontSize: theme.fontSizes[0],
+            color: theme.colors.secondary,
+            borderTop: `1px dotted ${theme.colors.hint}`,
+          },
+          statePropsActive: {
+            color: theme.colors.primary,
+            textDecoration: "underline",
+          },
+        }}
+      >
+        <GlobalStyles />
+        {children}
+      </StylesProvider>
     </ThemeProvider>
   );
 };
