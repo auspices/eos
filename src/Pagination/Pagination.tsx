be4d8b3d271cf28f107cdb8e3443223f79f2c28e
dzucconi <mail@damonzucconi.com>
2020-08-31 20:39:52 -0400

    feat(pagination): exports out common pagination helper

--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -15,6 +15,22 @@ export const PAGINATION_DEFAULT_PAGE = 1;
 export const PAGINATION_DEFAULT_PER = 24;
 export const PAGINATION_DEFAULT_INTERVAL = 3;
 
+export const paginate = ({
+  page = PAGINATION_DEFAULT_PAGE,
+  per = PAGINATION_DEFAULT_PER,
+  total,
+}: {
+  page: number;
+  per: number;
+  total: number;
+}) => {
+  const totalPages = Math.ceil(total / per);
+  const prevPage = page > 1 ? page - 1 : page;
+  const nextPage = page < totalPages ? page + 1 : page;
+
+  return { totalPages, prevPage, nextPage };
+};
+
 export const Pagination: React.FC<PaginationProps> = ({
   page = PAGINATION_DEFAULT_PAGE,
   per = PAGINATION_DEFAULT_PER,
@@ -24,9 +40,7 @@ export const Pagination: React.FC<PaginationProps> = ({
   Page = DefaultPage,
   ...rest
 }) => {
-  const totalPages = Math.ceil(total / per);
-  const prevPage = page > 1 ? page - 1 : page;
-  const nextPage = page < totalPages ? page + 1 : page;
+  const { totalPages, prevPage, nextPage } = paginate({ page, per, total });
 
   if (totalPages <= 1) return null;
 
