0798651134c9d245de1b5a004a00d0317aa9b87f
dzucconi <mail@damonzucconi.com>
2025-02-24 22:39:44 -0500

    Upgrades storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,21 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
+
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-viewport",
-  ]
+    "@storybook/addon-webpack5-compiler-swc"
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
