8f9f9a9e2dac767bd91741830bd48b8d023f7aed
dzucconi <mail@damonzucconi.com>
2025-03-16 22:23:17 -0400

    fix: improves default props typing

--- src/lib/withDefaultProps.tsx
+++ src/lib/withDefaultProps.tsx
@@ -1,27 +1,46 @@
 import React from "react";
 
-type AnyStyledComponent = {
-  (props: any): JSX.Element;
-  displayName?: string;
+type AsProp<C extends React.ElementType> = {
+  as?: C;
 };
 
-export function withDefaultProps<P extends object>(
-  WrappedComponent: AnyStyledComponent,
+type PropsWithAsProp<C extends React.ElementType, P = {}> = P &
+  AsProp<C> &
+  Omit<React.ComponentPropsWithRef<C>, keyof (AsProp<C> & P)>;
+
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
+
+type PropsWithRef<P, R, C extends React.ElementType> = PropsWithAsProp<C, P> & {
+  ref?: PolymorphicRef<C> | React.Ref<R>;
+};
+
+export function withDefaultProps<
+  P extends object,
+  R = any,
+  C extends React.ElementType = any,
+>(
+  WrappedComponent: React.ComponentType<PropsWithRef<P, R, C>>,
   defaultProps: Partial<P>,
   displayName?: string
-): AnyStyledComponent {
-  const WithDefaultProps = React.forwardRef<any, P>((props, ref) => {
-    const mergedProps = {
-      ...defaultProps,
-      ...props,
-    } as P;
+): React.ForwardRefExoticComponent<
+  React.PropsWithoutRef<PropsWithAsProp<C, P>> & React.RefAttributes<R>
+> {
+  const WithDefaultProps = React.forwardRef<R, PropsWithAsProp<C, P>>(
+    (props, ref) => {
+      const mergedProps = {
+        ...defaultProps,
+        ...props,
+        ref,
+      } as PropsWithRef<P, R, C>;
 
-    return <WrappedComponent ref={ref} {...mergedProps} />;
-  });
+      return <WrappedComponent {...mergedProps} />;
+    }
+  );
 
   WithDefaultProps.displayName =
     displayName ||
     `WithDefaultProps(${WrappedComponent.displayName || WrappedComponent.name})`;
 
-  return WithDefaultProps as AnyStyledComponent;
+  return WithDefaultProps;
 }
